ce1b9a536f39e51b1a39dfdf4f3e4299435fda02
Doctorwngd <94738518+Doctorwngd@users.noreply.github.com>
2024-09-20 23:10:33 +0800

    Update reduceDis_M_Ez.m

--- reduceDis_M_Ez.m
+++ reduceDis_M_Ez.m
@@ -1,31 +1,29 @@
 function Reduction_set = reduceDis_M_Ez(Dis_M)
 
-% 初始化Reduction_set为空集合
+% Initialize Reduction_set as an empty set
 Reduction_set = [];
 
 while true
-    % 按列求和
-    
+    % Sum across columns
     col_sum = sum(sum(Dis_M .^5, 1), 2);
     
-    % 获取最大值
+    % Get the maximum value
     [max_val, c_max] = max(col_sum);
-%     max_val
-    % 如果最大值是0，停止程序
+    
+    % If the maximum value is 0, stop the program
     if max_val == max(col_sum(Reduction_set))
-%     if max_val <= theta
         break;
     end
     
-    
-    % 将索引c_max加入Reduction_set
+    % Add the index c_max to the Reduction_set
     Reduction_set = [Reduction_set, c_max];
     
-    % 更新Dis_M
+    % Update Dis_M
     L_c_max = Dis_M(:, :, c_max);
-    Eliminate_c_max = L_c_max * -1 + 1;  % 将0变为1，1变为0
+    Eliminate_c_max = L_c_max * -1 + 1;  % Change 0 to 1, and 1 to 0
     Dis_M = Dis_M .* Eliminate_c_max;
    
 end
 
 end
+
